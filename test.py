1d1f5d2e4e4fedeccdc9a997b7b532c94035327c
Gerrit Koch <gerrit.koch@rwth-aachen.de>
2024-02-22 13:06:38 +0100

    added unit test for get_data()

---
+++ test.py
@@ -0,0 +1,13 @@
+import pytest
+
+import main
+
+
+def test_get_data_not_empty():
+    ticker = ['IBM', 'NVR', 'AZO', 'AVGO', 'ADBE', 'MSFT', 'GOOG', 'TEAM', 'HLT', 'AMZN', 'GRMN', 'OTTR', 'TEX', 'CSCO',
+              'BEPC', 'PSO', 'INFN', 'KODK', 'KRON', 'AMBO', 'WF', 'OBK', 'VVV', 'VET', 'DDD', 'CTRA', 'RRR', 'CSIQ']
+    assert not main.get_data(ticker_symbols=ticker).empty
+
+
+if __name__ == "__main__":
+    pytest.main()
